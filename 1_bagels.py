c87c2afcea8de429413b27b3ca9b9ac24f40f58e
Aakash Kumar <aakashkumar113450@gmail.com>
2025-11-12 12:12:17 +0530

    creating a bagels game

--- 1_bagels.py
+++ 1_bagels.py
@@ -1,88 +1,74 @@
 import random
 
-Num_digits = 2
-Max_guesses = 5
+ATTEMPT_LIMIT = 10
+GUESS_NUMBER = random.randint(100, 999)
+WANT_TO_PLAY_AGAIN = True 
 
-def main():
-    print("Bangles , a detective logic game.")
-    print("By Al, Sweigart al@inventwithpython.com\n")
-    print("I am thinking of a 10 digit number. Try to guess what is it.")
-    print("Here are some clew -")
-    print("\tWhen i say:        what it's mean:")
-    print("\tPico         One digit is correct but in wrong place")
-    print("\tFemi         One digit is correct and in right place")
-    print("\tBangles      No digit is correct")
+a = GUESS_NUMBER // 100
+b = (GUESS_NUMBER // 10) % 10
+c = GUESS_NUMBER % 10
 
-    while True: #main game loop.
-        secretNum = getSecretNum()
-        print('I have thought up a number. ')
-        print('You have {} guesses to get it.'.format(Max_guesses))
-        
-        numGuesses =1
+print(GUESS_NUMBER)
+print(a, b, c)
 
-        while numGuesses <= Max_guesses:
-            guess = ''
-            #     keep looping  until they enter a valid guess:
-            while len(guess) != Num_digits or not guess.isdecimal():
-                print('Guess #{}: '.format(numGuesses))
-                guess = input('>')
-                
-            clues = getClues(guess, secretNum)
-            print(clues)
-            numGuesses +=1
-            
-            if guess == secretNum:
-                break #They're correct , so break out of this loop.
-            if numGuesses > Max_guesses:
-                print('You ran out of guesses')
-                print('The answer was {}.'.format(secretNum))
-                
-        #Ask player if they want to play again
-        print('Do you want to play again? (yes or no)')
-        if not input('>').lower().startswith('y'):
-                     break
-                    
-    print('Thanks for playing !')
-    
-def getSecretNum():
-    """Returns a sting made up of Num_digits unique random digits."""
-    numbers = list('0123456789')   #Create a list of digits 0-9
-    random.shuffle(numbers) # shuffle them into random order.
-    
-    #Get the first Num_digits digits in the list for the secret number:
-    secretNum = ''
-    for i in range(Num_digits):
-        secretNum +=str(numbers[i])
-    return secretNum
 
-def getClues(guess, secretNum):
-    """Returns a string with the Pico, fermi, bagels clues for a guess and secret number pair."""
-    if guess ==secretNum:
-        return 'you got it!'
-    
-    clues = []
-    
-    for i in range(len(guess)):
-        if guess[i] == secretNum[i]:
-            #A correct digit is in the correct place.
-            clues.append('Fermi')
-        elif guess[i] in secretNum:
-            #A correct digit is in the incorrect place.
-            clues.append('Pico')
-    if len(clues) == 0:
-        return 'Bangles' #There are no correct digits at all.
-    else:
-        #sort the clues into alphetical order so their original order
-        #doesn't give information away.
-        clues.sort()
-        #Make a single string from the list of string clues.
-        return ''.join(clues)
+def check_number(d1: int, d2: int, d3: int) -> str:
 
-#if the program is run (instead of imported), run the game:
-if __name__ == '__main__':
-    main()
+    if a == d1 and b == d2 and c == d3:
+        return "You got it!"
 
-    
-    
-            
-        
+    result = ""
+    for i in [d1, d2, d3]:
+        print(i)
+        if i == a:
+            result += "Femi "
+        elif i in [a, b, c]:
+            result += "Pico "
+        else: 
+            result += "Bangles "
+
+    return result
+
+
+print("**** Bagels, a deductive logic game. ****")
+print("I am thinking of a 3-digit number. Try to guess what it is.")
+print("Here are some clues:")
+print("""
+\tWhen I say: That means:
+\tPico One digit is correct but in the wrong position.
+\tFermi One digit is correct and in the right position.
+\tBagels No digit is correct.\n
+""")
+
+print(
+    "I have thought up a number.\
+You have 10 guesses to get it.\
+"
+)
+
+while WANT_TO_PLAY_AGAIN:
+    for i in range(1, ATTEMPT_LIMIT + 1):
+        print(f"Guess #{i}:")
+        mynumber = input(">")
+        try:
+            # This splits the string AND converts each character to an int
+            d1, d2, d3 = map(int, mynumber)
+
+        except ValueError as e:
+            print(f"Invalid input: {e}")
+            print("Please enter exactly 3 digits.")
+            continue
+
+        display_string = check_number(d1, d2, d3)
+        print(display_string)
+        if display_string == "You got it!":
+            break
+
+    print("Do you want to play again? (yes or no)")
+    is_user_want_to_play = input(">")
+
+    if is_user_want_to_play not in ["yes", "y", "ok", "sure", "yup"]:
+        WANT_TO_PLAY_AGAIN = False
+
+
+print("Thanks for playing!")
